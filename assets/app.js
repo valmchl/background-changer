3422d0d9f89b521d71c14865605c206f77a1924b
Valerie <32314887+VGMichel@users.noreply.github.com>
2022-02-27 13:13:23 -0500

    Fix custom jQuery

--- assets/app.js
+++ assets/app.js
@@ -16,19 +16,19 @@ $(document).ready(function(){
 		$('custombg',"bg_custom",{ expires: 365, path: '/'});
 		var customurl = $("#custom__input").val();
 		$('custom__url',customurl,{ expires: 365, path: '/'});
-		$("<style type='text/css'>body#bg_custom, #bg_custom .maintitle, #bg_custom #header, #bg_custom .footer{ background-image: url(" + customurl + ")}</style>").appendTo("head");
+		$("<style type='text/css'>body#bg_custom{ background-image: url(" + customurl + ")}</style>").appendTo("head");
 		$("body").removeClass().addClass("bg_custom");
 	});
 		
 	if ( (('custombg') != null))	{
-		$("body").addClass(('custombg'));
+		$("body").addClass('custombg');
 	}
 	else{
 		$("body").addClass("bg0");
 	}
 	
 	if ( (('custom_url') != null))	{
-		$("<style type='text/css'>body#bg_custom, #bg_custom .maintitle, #bg_custom #header, #bg_custom .footer{ background-image: url(" + $('custom_url') + ")}</style>").appendTo("head");
+		$("<style type='text/css'>body#bg_custom{ background-image: url(" + $('custom_url') + ")}</style>").appendTo("head");
 		$("body").addClass("bg_custom");
 	}
 	
