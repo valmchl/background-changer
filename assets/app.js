33dae1587fc17ab6c18c92ad65bf29f5227e1cc3
Valerie <32314887+VGMichel@users.noreply.github.com>
2022-03-08 21:21:15 -0500

    Final Commit

--- assets/app.js
+++ assets/app.js
@@ -2,34 +2,17 @@ $(document).ready(function(){
 	
 	
 	$(".fa-image").click(function(){
-		$("#toggle_background").slideToggle();
+		$("#toggle__background").slideToggle();
 	});
 	
 	$("#custom__background span").click(function(){
-		$('custom__url',null,{ expires: -5, path: '/'});
-		var bgid = $(this).attr("id");
-		$('custombg',bgid,{ expires: 365, path: '/'});
+		const bgid = $(this).attr("id");
 		$("body").removeClass().addClass(bgid);
 	});
 	
 	$("#custom__submit").click(function(){
-		$('custombg',"bg_custom",{ expires: 365, path: '/'});
-		var customurl = $("#custom__input").val();
-		$('custom__url',customurl,{ expires: 365, path: '/'});
-		$("<style type='text/css'>body#bg_custom{ background-image: url(" + customurl + ")}</style>").appendTo("head");
+		const customurl = $("#custom__input").val();
+		$("<style type='text/css'>body{ background-image: url(" + customurl + ")}</style>").appendTo("head");
 		$("body").removeClass().addClass("bg_custom");
 	});
-		
-	if ( (('custombg') != null))	{
-		$("body").addClass('custombg');
-	}
-	else{
-		$("body").addClass("bg0");
-	}
-	
-	if ( (('custom_url') != null))	{
-		$("<style type='text/css'>body#bg_custom{ background-image: url(" + $('custom_url') + ")}</style>").appendTo("head");
-		$("body").addClass("bg_custom");
-	}
-	
 });
