b1caf9e055aa6450feb3485341568e2d2f0a997f
Valerie <32314887+VGMichel@users.noreply.github.com>
2023-01-22 16:38:50 -0500

    Add kawaii background images from vecteezy; update CSS

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-	
-	
-	$(".fa-image").click(function(){
+$(document).ready(function () {
+
+
+	$(".fa-chevron-down").click(function () {
 		$("#toggle__background").slideToggle();
 	});
-	
-	$("#custom__background span").click(function(){
+
+	$("#custom__background span").click(function () {
 		const bgid = $(this).attr("id");
 		$("body").removeClass().addClass(bgid);
 	});
-	
-	$("#custom__submit").click(function(){
+
+	$("#custom__submit").click(function () {
 		const customurl = $("#custom__input").val();
 		$("<style type='text/css'>body{ background-image: url(" + customurl + ")}</style>").appendTo("head");
 		$("body").removeClass().addClass("bg_custom");
